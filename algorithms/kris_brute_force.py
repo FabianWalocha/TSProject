30cbf9cd6c5b68529f6265e9141f1e6ffbb2f2fb
Kris Kulivnyk <43318715+KrisKuliv@users.noreply.github.com>
2018-11-28 19:50:13 +0200

    Update kris_brute_force.py

--- algorithms/kris_brute_force.py
+++ algorithms/kris_brute_force.py
@@ -8,7 +8,6 @@ from operator import itemgetter
 
 
 #Styles for plot
-#%matplotlib inline
 plt.style.use('seaborn-notebook')
 
 
@@ -16,32 +15,24 @@ plt.style.use('seaborn-notebook')
 def bruteForce(coordinates):
     #constants to generate data
     DOTS_N  = coordinates.size()
-    DIST_RANGE = 8
+    #DIST_RANGE = 8
     fr_fr_faker = Faker('fr_FR')  # set French facker
 
     #Generator
     dots_pure = coordinates
     listofcitynames = [fr_fr_faker.city() for x in range(DOTS_N)]
 
+    '''
     #Build distance matrix and Data Frame (for test, are not used anywhere for now)
     array_dots = np.array(dots_pure)
     M=[[math.sqrt(sum([(array_dots[i][d]-array_dots[j][d])**2 for d in[0,1]]))for j in range(DOTS_N)]for i in range(DOTS_N)]
     df = pd.DataFrame(np.matrix(M), columns=listofcitynames, index=listofcitynames)
-    #print(df)
-
-        #Plot dots with labels
-    plt.plot([dots_pure[i][0]for i in range(DOTS_N)], [dots_pure[i][1]for i in range(DOTS_N)],'c8')
-    for i in range(DOTS_N):
-        plt.annotate(
-            listofcitynames[i],
-            xy=(dots_pure[i][0], dots_pure[i][1]))
-    #plt.show()
-
+    '''
+    
     #Combine city names and dots
     dots =[[listofcitynames[i],dots_pure[i]] for i in range(DOTS_N)]
 
     #Generate all possible unique permutations
-    #print("Number of possible permutations = ", math.factorial(DOTS_N))
     possibleWays=list(multiset_permutations(dots))
 
     time_start = time.time()
@@ -52,13 +43,8 @@ def bruteForce(coordinates):
 
     #Get minimum distance and path
     min_output = sorted(output, key=itemgetter(0))[0]
-    #print('Min Distance = ', min_output[0])
     distance =  min_output[0]
-    #print('Min Path = ', min_output[1])
     time_end = time.time()
 
     execution_time = time_end - time_start
-    #print("Execution time", execution_time)
-    #print('Middle distance = ', sorted(output, key=itemgetter(0))[5038])
-    #print(sorted(output, key=itemgetter(0)))
     return (execution_time,distance)
