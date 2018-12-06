693a3879abe73c5bce3590dc6f0cb6b2bdc1957f
edbrandao <39321150+edbrandao@users.noreply.github.com>
2018-12-06 22:59:24 +0100

    Major rewrite ALGO_simulated_annealing.py
    - Major rewrite (which kinda looks the same); after extensive testing and tweaking, it now outperform greedy in all cases, for all size graphs; if it doesn't, set greedy_start = False.
    - Finally, set return_graph = True for some nice insights.

--- algorithms/ALGO_simulated_annealing.py
+++ algorithms/ALGO_simulated_annealing.py
@@ -1,82 +1,87 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-__author__ = "Eduardo Brandao"
-__copyright__ = "Copyright 2018"
-__status__ = "Prototype"
-
 import graph_class as g
 import itertools
 import numpy as np
 import time
+import random as rd
+from matplotlib import pyplot as plt
 
-
-# In[2]:
-
-#returns elapsed time, the minimum distance, and the minimum cycle/plot of the cycle
-def simulated_annealing(graph,random_init = True, temperature = 10000, cooling_factor = 0.999, return_graph = False, method = "2-OPT", break_time = 600):
+def greedy_just_for_sa(graph):
     """"
-    graph: instance of class graph; can be created from coordinate list (graph.fully_connected_graph_from_coordinate_list),
-    from an adjacency matrix (graph.graph_from_adjacency_matrix) or from a tsp type file (graph.heidelberg_2D)
-    random_init: set to True, starts from a random cycle; otherwise it starts from implementation sequence cycle
-    temperature: initial "temperature" of the graph. The greater it is, the greater the possibility of an inferior cycle 
-    being accepted
-    return_graph: set to True, displays a plot of the graph; otherwise, it returns the vertices (class vertex objects)
-    """
-    #ISSUES:
-    #1. Cools one degree for every computation step. Should be decoupled: simulate cooling time using a "cooling function"
-    # DONE, using Kirpatrick cooling
-    #2. Instead of shuffling a random path, should maximize the distance between the two permutations (since the length 
-        # of the sequence to be shuffled is already random). This could be done using, perhaps, the "Kendal tau distance":
-        # [[https://en.wikipedia.org/wiki/Kendall_tau_distance]]
-    # DONE: there's no such thing; I just chose another neighbouring method (2-OPT), which is faster to compute
+    Thanks Fabian!
+    """"
+    adj_mat = graph.weighted_adjacency_matrix
+    nNodes = len(adj_mat)
+    firstMove = np.unravel_index(np.argmin(adj_mat),adj_mat.shape)
+    listOfTraversedNodes = [firstMove[0],firstMove[1]]
+    cost = adj_mat[firstMove[0],firstMove[1]]
+    while len(listOfTraversedNodes) < len(adj_mat):
+        currentCosts = list(adj_mat[listOfTraversedNodes[-1],:])
+        for idx2 in listOfTraversedNodes:
+            currentCosts[idx2] = np.inf
+        listOfTraversedNodes.append(np.argmin(currentCosts))
+        cost += adj_mat[listOfTraversedNodes[-2],listOfTraversedNodes[-1]]
+    cost += adj_mat[listOfTraversedNodes[-1],listOfTraversedNodes[0]]
+    return listOfTraversedNodes
+
+
+
+def simulated_annealing(graph, iterations=0, greedy_start = True, temperature=10,
+           cooling_factor = 0.99999, return_graph = False, method = "2-OPT", break_time = 600):
     
     start = time.time()
     
-    if random_init:
-        nodes = np.random.permutation(graph.vertices)
+    if greedy_start:
+        nodes = [graph.vertices[index] for index in greedy_just_for_sa(graph)]
     else:
-        nodes = graph.vertices
-        
+        nodes = np.random.permutation(graph.vertices)
+    
+    
     current_min = graph.get_cycle_weight(nodes)
     
-    # with default temperature and cooling rate, it will go through about 23000 iterations,
-    while temperature>0.000001:
-        
-        k = np.random.randint(0,len(nodes))
-        p = np.random.randint(k,len(nodes))
-        
-        # this is the NEIGHBOURING algorithm in the pseudocode        
-        # we can choose to expore the problem space by either
-        # doing a random SHUFFLE of the nodes between k and p or a full inversion (2-OPT)
-        # 2-OPT should be faster to compute, but the set of neighbours is smaller at each iteration
-        # there are paths in solution space that are not accessible to 2-OPT, which are accessible to SHUFFLE;
-        # there should be some classes of problems for which each method converges faster
-        
-        if method == "SHUFFLE":
-            np.random.shuffle(nodes[k:p])
-        elif method == "2-OPT":    
-            nodes[k:p]= np.flip(nodes[k:p], axis=0)
-        
-        test_min = graph.get_cycle_weight(nodes)
-        
-        # Replaced Gaussian with Boltzmann distribution, in the spirit of good physics ;)
-        if np.exp(-(test_min-current_min)/(temperature))> np.random.rand():
-            current_min = test_min
-        else:
-            current_min = min(current_min, test_min)
-        if time.time()-start > break_time:
-            break
-        # replaced linear cooling with kirpatrick cooling(http://citeseer.ist.psu.edu/kirkpatrick83optimization.html),
-        # again in the spirit of good physics and because we want the algorithm to spend enough time perfecting the 
-        # local minimum
-        temperature = temperature*cooling_factor
-    
+    while temperature>1:
+        #difference in paths can't be greater than one
+        #for very small T, e^{-\DeltaE/T} is going accept small \Delta E with great likelihood!
+
+            #generate random nodes 
+            k = np.random.randint(0,len(nodes))
+            p = np.random.randint(k,len(nodes))
+
+            test_nodes = nodes.copy()
+            
+            if method == "SHUFFLE":
+                np.random.shuffle(test_nodes[k:p])
+            elif method == "2-OPT":
+                test_nodes[k:p] = np.flip(test_nodes[k:p], axis=0)
+
+            current_path_length = graph.get_cycle_weight(nodes)
+            test_path_length = graph.get_cycle_weight(test_nodes)
+
+            # Boltzmann distribution
+            Delta_Length = test_path_length - current_path_length
+
+            p = min(np.exp(-(Delta_Length)/(temperature)),1)
+            
+            # testing a few hundred runs shows that <= is marginally better on average,
+            # but better at finding minima, since variance tends to be greater
+            # I believe this happens because always accepting same-length configurations
+            # allows it to explore the solution space better
+            if Delta_Length <= 0:
+                nodes = test_nodes.copy()
+
+            elif p > np.random.rand():
+                    nodes = test_nodes.copy()
+            #kirpatrick cooling(http://citeseer.ist.psu.edu/kirkpatrick83optimization.html)
+            temperature = temperature*cooling_factor
+            if iterations>0:
+                if temperature <1:
+                    temperature = 100
+                    iterations-=1
+            
+            if time.time()-start > break_time:
+                break
     end = time.time()
     if return_graph:
         node_output = g.plotTSP_2D(nodes)
     else:
         node_output = nodes
-    return end-start, current_min, node_output
+    return end-start, graph.get_cycle_weight(nodes), node_output
