738a1f36e2ad1147df9d368acaa73075d10bfd65
fwalocha <uk058871@uni-kassel.de>
2018-12-10 07:21:49 +0100

    added plots, changed distance func in bfm and bfk

--- algorithms/kris_brute_force.py
+++ algorithms/kris_brute_force.py
@@ -25,7 +25,7 @@ def bruteForce(coordinates):
 
     #Build distance matrix and Data Frame (for test, are not used anywhere for now)
     array_dots = np.array(dots_pure)
-    M=[[math.sqrt(sum([(array_dots[i][d]-array_dots[j][d])**2 for d in[0,1]]))for j in range(DOTS_N)]for i in range(DOTS_N)]
+    M=[[int(np.around(math.sqrt(sum([(array_dots[i][d]-array_dots[j][d])**2 for d in[0,1]]))))for j in range(DOTS_N)]for i in range(DOTS_N)]
     df = pd.DataFrame(np.matrix(M), columns=listofcitynames, index=listofcitynames)
     #print(df)
 
@@ -46,7 +46,7 @@ def bruteForce(coordinates):
 
     time_start = time.time()
     #Calculate distances, combine appropriate pathes
-    ways_distance = [sum([math.sqrt(sum([(possibleWays[j][i+1][1][d]-possibleWays[j][i][1][d])**2 for d in[0,1]]))for i in range(DOTS_N-1)]) + sum([math.sqrt(sum([(possibleWays[j][DOTS_N-1][1][d]-possibleWays[j][0][1][d])**2 for d in[0,1]]))]) for j in range(len(possibleWays)-1)]
+    ways_distance = [sum([int(np.around(math.sqrt(sum([(possibleWays[j][i+1][1][d]-possibleWays[j][i][1][d])**2 for d in[0,1]]))))for i in range(DOTS_N-1)]) + sum([int(np.around(math.sqrt(sum([(possibleWays[j][DOTS_N-1][1][d]-possibleWays[j][0][1][d])**2 for d in[0,1]]))))]) for j in range(len(possibleWays)-1)]
     ways_path = [[possibleWays[j][i][0] for i in range(DOTS_N)]for j in range(len(possibleWays)-1)]
     output =[[ways_distance[i],ways_path[i]] for i in range(len(possibleWays)-1)]
 
